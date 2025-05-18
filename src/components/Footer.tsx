2de2e8c4dd33bee0210f89096887c8052017d4bd
Rafael Impas <impasrafael@gmail.com>
2025-05-18 15:37:38 +0800

    Added logo for favicon, navbar, and footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import { Facebook, Globe2, Instagram, Twitter } from "lucide-react";
 const Footer = () => {
   return (
     <div className="bg-green-primary h-[300px] md:h-[200px] w-screen flex flex-col md:flex-row justify-center items-center px-10 gap-2 md:gap-0">
-      <div className="flex md:w-[50%] justify-center">
+      <div className="flex md:w-[50%] justify-center items-center">
+        <img src="/logo.svg" alt="logo" className="h-[8vh]" />
         <h1 className="text-[3rem] font-bold text-white">Eat's Good!</h1>
       </div>
       <div className="flex flex-col w-[50%] justify-baseline gap-4">
